ac6a21203b2e411329af155dd04397c62555ba72
Brandon Royal <brr@sitecore.net>
2015-09-22 17:43:48 -0700

    updated

--- src/Sitecore.Poc.CustomEntityService/Controllers/MobileMenuController.cs
+++ src/Sitecore.Poc.CustomEntityService/Controllers/MobileMenuController.cs
@@ -1,7 +1,17 @@
-﻿namespace Sitecore.Poc.CustomEntityService.Controllers
-{
-    public class MobileMenuController
-    {
+﻿using Sitecore.Poc.CustomEntityService.Entities;
+using Sitecore.Poc.CustomEntityService.Repositories;
+using Sitecore.Services.Core;
+using Sitecore.Services.Infrastructure.Sitecore.Services;
 
+namespace Sitecore.Poc.CustomEntityService.Controllers
+{
+    [ServicesController("menu/mobile")]
+    public class MobileMenuController : EntityService<MenuItem>
+    {
+        public MobileMenuController(IRepository<MenuItem> repository) : base(repository)
+        { }
+
+        public MobileMenuController() : this(new MenuRespository<Mobile>())
+        { }
     }
 }
